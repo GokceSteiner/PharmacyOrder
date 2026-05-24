264ee31ab74ccdf51d521ac2b3ea19e39490adb2
GokceSteiner <STE250732@spengergasse.at>
2026-05-24 14:58:35 +0200

    Add home and price view. No cards.

--- src/main/java/at/spengergasse/views/home/HomeView.java
+++ src/main/java/at/spengergasse/views/home/HomeView.java
@@ -1,8 +1,7 @@
 package at.spengergasse.views.home;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
@@ -18,19 +17,54 @@ public class HomeView extends VerticalLayout {
     public HomeView() {
         setSpacing(false);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+        setAlignItems(Alignment.CENTER);
 
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        H1 company = new H1("Steiner Pharmacy");
+        company.getStyle()
+                .set("font-family", "cursive")
+                .set("font-size", "6rem")
+                .set("margin", "0");
 
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        H2 subName = new H2("... Trusted care, fast service, better health ...");
+        subName.getStyle()
+                .set("margin", "0")
+                .set("color", "gray");
+
+        Image logo = new Image("images/logo.png", "Steiner Pharmacy Logo");
+        logo.setWidth("300px");
+
+        Paragraph line1 = new Paragraph("Willkommen bei Steiner Pharmacy – Ihrer vertrauensvollen Apotheke für Gesundheit, Wohlbefinden und persönliche Beratung. Unser Ziel ist es, Sie mit hochwertigen pharmazeutischen Produkten, kompetenter Betreuung und einem zuverlässigen Service bestmöglich zu unterstützen. Ihre Gesundheit steht bei uns an erster Stelle.");
+
+        line1.setWidth("500px");
+        line1.getStyle()
+                .set("font-size", "22px")
+                .set("line-height", "1.6")
+                .set("text-align", "left");
+
+        Paragraph line2 = new Paragraph("Bei Steiner Pharmacy verbinden wir moderne pharmazeutische Versorgung mit persönlicher Betreuung. Ob Medikamente, Gesundheitsprodukte oder individuelle Beratung – unser engagiertes Team steht Ihnen mit Fachwissen und Sorgfalt zur Seite. Wir legen großen Wert auf Qualität, Sicherheit und ein angenehmes Kundenerlebnis.");
+
+        line2.setWidth("500px");
+        line2.getStyle()
+                .set("font-size", "22px")
+                .set("line-height", "1.6")
+                .set("text-align", "left");
+
+        Paragraph line3 = new Paragraph("Als Ihre moderne Apotheke möchten wir Gesundheit einfach und zugänglich machen. Mit einem benutzerfreundlichen Service, schnellen Lösungen und einem starken Fokus auf Ihre Bedürfnisse ist Steiner Pharmacy Ihr zuverlässiger Partner für ein gesundes Leben – heute und in Zukunft.");
+
+        line3.setWidth("500px");
+        line3.getStyle()
+                .set("font-size", "22px")
+                .set("line-height", "1.6")
+                .set("text-align", "left");
+
+
+
+        H3 name = new H3("Steiner Pharmacy");
+        H3 street = new H3("Spengergasse 20");
+        H3 city = new H3("1050 Wien");
+        
+        add(company, subName, logo, line1 , line2, line3, name,street,city);
+
     }
 
 }
--- src/main/java/at/spengergasse/views/prices/PricesView.java
+++ src/main/java/at/spengergasse/views/prices/PricesView.java
@@ -1,5 +1,6 @@
 package at.spengergasse.views.prices;
 
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
@@ -18,19 +19,57 @@ public class PricesView extends VerticalLayout {
     public PricesView() {
         setSpacing(false);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+        H1 company = new H1("Steiner Pharmacy");
+        company.getStyle()
+                .set("font-family", "cursive")
+                .set("font-size", "6rem")
+                .set("margin", "0");
 
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        H2 subName = new H2("... Trusted care, fast service, better health ...");
+        subName.getStyle()
+                .set("margin", "0")
+                .set("color", "gray");
 
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        H2 pricelist = new H2("Unsere Preisgruppen");
+        pricelist.getStyle()
+                .set("margin", "0")
+                .set("color", "gray");
+
+        H2 group1 = new H2("Schmerzmittel");
+        Paragraph g1p1 = new Paragraph("Ibuprofen, Paracetamol, Aspirin");
+        Paragraph g1p2 = new Paragraph("Schnelle Hilfe bei Schmerzen und Fieber");
+        Paragraph g1p3 = new Paragraph("Preise ab 4,90 Euro");
+
+        H2 group2 = new H2("Erkältung & Grippe");
+        Paragraph g2p1 = new Paragraph("Hustensaft, Nasenspray, Halstabletten");
+        Paragraph g2p2 = new Paragraph("Für schnelle Linderung bei Erkältungssymptomen");
+        Paragraph g2p3 = new Paragraph("Preise ab 7,90 Euro");
+
+        H2 group3 = new H2("Vitamine & Nahrungsergänzung");
+        Paragraph g3p1 = new Paragraph("Vitamin C, Magnesium, Omega-3");
+        Paragraph g3p2 = new Paragraph("Unterstützung für Immunsystem und Wohlbefinden");
+        Paragraph g3p3 = new Paragraph("Preise ab 9,90 Euro");
+
+        H2 group4 = new H2("Hautpflege & Dermatologie");
+        Paragraph g4p1 = new Paragraph("Cremes, Salben, Sonnenschutz");
+        Paragraph g4p2 = new Paragraph("Pflege für empfindliche und trockene Haut");
+        Paragraph g4p3 = new Paragraph("Preise ab 12,90 Euro");
+
+        H2 group5 = new H2("Rezeptpflichtige Medikamente");
+        Paragraph g5p1 = new Paragraph("Antibiotika, Blutdruckmedikamente, Spezialpräparate");
+        Paragraph g5p2 = new Paragraph("Nur mit ärztlicher Verschreibung erhältlich");
+        Paragraph g5p3 = new Paragraph("Preis auf Anfrage");
+
+        add(company, subName, pricelist,
+                group1, g1p1, g1p2, g1p3,
+
+                group2, g2p1, g2p2, g2p3,
+
+                group3, g3p1, g3p2, g3p3,
+
+                group4, g4p1, g4p2, g4p3,
+
+                group5, g5p1, g5p2, g5p3);
     }
 
 }
