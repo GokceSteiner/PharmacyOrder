c68d700bc57b325ee9d82f3d14be518c8ede2b10
GokceSteiner <STE250732@spengergasse.at>
2026-05-24 17:08:23 +0200

    Add Cards.

--- src/main/java/at/spengergasse/views/home/HomeView.java
+++ src/main/java/at/spengergasse/views/home/HomeView.java
@@ -2,6 +2,7 @@ package at.spengergasse.views.home;
 
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
@@ -9,15 +10,73 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 import org.vaadin.lineawesome.LineAwesomeIconUrl;
 
+import static com.vaadin.copilot.shaded.io.netty.handler.codec.http.HttpHeaders.getHeader;
+
 @PageTitle("Home")
 @Route("home")
 @Menu(order = 0, icon = LineAwesomeIconUrl.HOME_SOLID)
 public class HomeView extends VerticalLayout {
 
-    public HomeView() {
+    public HomeView()
+    {
         setSpacing(false);
 
-        setAlignItems(Alignment.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+
+       VerticalLayout header = getHeader();
+        HorizontalLayout logoText = new HorizontalLayout();
+
+
+        Image logo = new Image("images/logo.png", "Steiner Pharmacy Logo");
+        logo.setWidth("400px");
+
+        Paragraph line1 = new Paragraph("Willkommen bei Steiner Pharmacy – Ihrer vertrauensvollen Apotheke für Gesundheit, Wohlbefinden und persönliche Beratung. Unser Ziel ist es, Sie mit hochwertigen pharmazeutischen Produkten, kompetenter Betreuung und einem zuverlässigen Service bestmöglich zu unterstützen. Ihre Gesundheit steht bei uns an erster Stelle.");
+
+        line1.setWidth("500px");
+        line1.getStyle()
+                .set("font-size", "22px")
+                .set("line-height", "1.6")
+                .set("text-align", "left");
+
+        logoText.add(logo, line1);
+
+        Paragraph line2 = new Paragraph("Bei Steiner Pharmacy verbinden wir moderne pharmazeutische Versorgung mit persönlicher Betreuung. Ob Medikamente, Gesundheitsprodukte oder individuelle Beratung – unser engagiertes Team steht Ihnen mit Fachwissen und Sorgfalt zur Seite. Wir legen großen Wert auf Qualität, Sicherheit und ein angenehmes Kundenerlebnis.");
+
+        line2.setWidth("500px");
+        line2.getStyle()
+                .set("font-size", "22px")
+                .set("line-height", "1.6")
+                .set("margin-top", "30px")
+                .set("text-align", "left");
+
+
+        Paragraph line3 = new Paragraph("Als Ihre moderne Apotheke möchten wir Gesundheit einfach und zugänglich machen. Mit einem benutzerfreundlichen Service, schnellen Lösungen und einem starken Fokus auf Ihre Bedürfnisse ist Steiner Pharmacy Ihr zuverlässiger Partner für ein gesundes Leben – heute und in Zukunft.");
+
+        line3.setWidth("500px");
+        line3.getStyle()
+                .set("font-size", "22px")
+                .set("line-height", "1.6")
+                .set("text-align", "left");
+
+        HorizontalLayout address = new HorizontalLayout();
+        H3 name = new H3("Steiner Pharmacy");
+        H3 street = new H3("Spengergasse 20");
+        H3 city = new H3("1050 Wien");
+
+        address.add(name,street,city);
+
+        add(header, logoText, line2, line3, address);
+
+    }
+
+    public static VerticalLayout getHeader()
+    {
+        VerticalLayout header;
+
+        header = new VerticalLayout();
+        header.setAlignItems(FlexComponent.Alignment.CENTER);
+        header.setWidthFull();
+
 
         H1 company = new H1("Steiner Pharmacy");
         company.getStyle()
@@ -30,41 +89,8 @@ public class HomeView extends VerticalLayout {
                 .set("margin", "0")
                 .set("color", "gray");
 
-        Image logo = new Image("images/logo.png", "Steiner Pharmacy Logo");
-        logo.setWidth("300px");
+        header.add(company, subName);
 
-        Paragraph line1 = new Paragraph("Willkommen bei Steiner Pharmacy – Ihrer vertrauensvollen Apotheke für Gesundheit, Wohlbefinden und persönliche Beratung. Unser Ziel ist es, Sie mit hochwertigen pharmazeutischen Produkten, kompetenter Betreuung und einem zuverlässigen Service bestmöglich zu unterstützen. Ihre Gesundheit steht bei uns an erster Stelle.");
-
-        line1.setWidth("500px");
-        line1.getStyle()
-                .set("font-size", "22px")
-                .set("line-height", "1.6")
-                .set("text-align", "left");
-
-        Paragraph line2 = new Paragraph("Bei Steiner Pharmacy verbinden wir moderne pharmazeutische Versorgung mit persönlicher Betreuung. Ob Medikamente, Gesundheitsprodukte oder individuelle Beratung – unser engagiertes Team steht Ihnen mit Fachwissen und Sorgfalt zur Seite. Wir legen großen Wert auf Qualität, Sicherheit und ein angenehmes Kundenerlebnis.");
-
-        line2.setWidth("500px");
-        line2.getStyle()
-                .set("font-size", "22px")
-                .set("line-height", "1.6")
-                .set("text-align", "left");
-
-        Paragraph line3 = new Paragraph("Als Ihre moderne Apotheke möchten wir Gesundheit einfach und zugänglich machen. Mit einem benutzerfreundlichen Service, schnellen Lösungen und einem starken Fokus auf Ihre Bedürfnisse ist Steiner Pharmacy Ihr zuverlässiger Partner für ein gesundes Leben – heute und in Zukunft.");
-
-        line3.setWidth("500px");
-        line3.getStyle()
-                .set("font-size", "22px")
-                .set("line-height", "1.6")
-                .set("text-align", "left");
-
-
-
-        H3 name = new H3("Steiner Pharmacy");
-        H3 street = new H3("Spengergasse 20");
-        H3 city = new H3("1050 Wien");
-        
-        add(company, subName, logo, line1 , line2, line3, name,street,city);
-
+        return header;
     }
-
 }
--- src/main/java/at/spengergasse/views/prices/PricesView.java
+++ src/main/java/at/spengergasse/views/prices/PricesView.java
@@ -1,9 +1,13 @@
 package at.spengergasse.views.prices;
 
+import at.spengergasse.views.home.HomeView;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
@@ -19,57 +23,53 @@ public class PricesView extends VerticalLayout {
     public PricesView() {
         setSpacing(false);
 
-        H1 company = new H1("Steiner Pharmacy");
-        company.getStyle()
-                .set("font-family", "cursive")
-                .set("font-size", "6rem")
-                .set("margin", "0");
-
-        H2 subName = new H2("... Trusted care, fast service, better health ...");
-        subName.getStyle()
-                .set("margin", "0")
-                .set("color", "gray");
+        VerticalLayout header = HomeView.getHeader();
 
         H2 pricelist = new H2("Unsere Preisgruppen");
         pricelist.getStyle()
                 .set("margin", "0")
                 .set("color", "gray");
 
-        H2 group1 = new H2("Schmerzmittel");
-        Paragraph g1p1 = new Paragraph("Ibuprofen, Paracetamol, Aspirin");
-        Paragraph g1p2 = new Paragraph("Schnelle Hilfe bei Schmerzen und Fieber");
-        Paragraph g1p3 = new Paragraph("Preise ab 4,90 Euro");
 
-        H2 group2 = new H2("Erkältung & Grippe");
-        Paragraph g2p1 = new Paragraph("Hustensaft, Nasenspray, Halstabletten");
-        Paragraph g2p2 = new Paragraph("Für schnelle Linderung bei Erkältungssymptomen");
-        Paragraph g2p3 = new Paragraph("Preise ab 7,90 Euro");
+        FlexLayout groupsHor = new FlexLayout();
 
-        H2 group3 = new H2("Vitamine & Nahrungsergänzung");
-        Paragraph g3p1 = new Paragraph("Vitamin C, Magnesium, Omega-3");
-        Paragraph g3p2 = new Paragraph("Unterstützung für Immunsystem und Wohlbefinden");
-        Paragraph g3p3 = new Paragraph("Preise ab 9,90 Euro");
+        VerticalLayout group1 = getCard("Schmerzmittel", 8.60,4.25,10.50);
+        VerticalLayout group2 = getCard("Erkältung & Grippe", 23.50,11.25, 28.99);
+        VerticalLayout group3 = getCard("Vitamine & Nahrungsergänzung", 35.25,18.70, 36.50);
+        VerticalLayout group4 = getCard("Hautpflege & Dermatologie", 13.25, 7.99, 15.25);
+        VerticalLayout group5 = getCard("Pharmazeutische Spezialprodukte", 125.00, 75.45, 150.00);
 
-        H2 group4 = new H2("Hautpflege & Dermatologie");
-        Paragraph g4p1 = new Paragraph("Cremes, Salben, Sonnenschutz");
-        Paragraph g4p2 = new Paragraph("Pflege für empfindliche und trockene Haut");
-        Paragraph g4p3 = new Paragraph("Preise ab 12,90 Euro");
+        groupsHor.setWidthFull();
+        groupsHor.setJustifyContentMode(JustifyContentMode.CENTER);
+        groupsHor.setFlexWrap(FlexLayout.FlexWrap.WRAP);
+        groupsHor.add(group1, group2, group3, group4, group5);
 
-        H2 group5 = new H2("Rezeptpflichtige Medikamente");
-        Paragraph g5p1 = new Paragraph("Antibiotika, Blutdruckmedikamente, Spezialpräparate");
-        Paragraph g5p2 = new Paragraph("Nur mit ärztlicher Verschreibung erhältlich");
-        Paragraph g5p3 = new Paragraph("Preis auf Anfrage");
 
-        add(company, subName, pricelist,
-                group1, g1p1, g1p2, g1p3,
+        Paragraph info = new Paragraph("Inklusive Steuer");
 
-                group2, g2p1, g2p2, g2p3,
+        add(header, pricelist, groupsHor, info);
+    }
 
-                group3, g3p1, g3p2, g3p3,
+    public VerticalLayout getCard(String titleText, double ohneRezept, double mitRezept, double nachtDienst)
+    {
+        VerticalLayout group = new VerticalLayout();
+        H2 groupName = new H2(titleText);
+        Paragraph preisOhneRezept = new Paragraph("Preis ohne Rezept " + ohneRezept + " €");
+        Paragraph preisMitRezept = new Paragraph("Preis mit Rezept " + mitRezept + " €");
+        Paragraph preisNachtDienst = new Paragraph("Preis Nachdienst " + nachtDienst +" €");
 
-                group4, g4p1, g4p2, g4p3,
+        group.add(groupName, preisOhneRezept, preisMitRezept, preisNachtDienst);
 
-                group5, g5p1, g5p2, g5p3);
+        group.setWidth("350px");
+        group.setPadding(true);
+        group.setSpacing(false);
+        group.getStyle()
+                .set("border", "1px solid lightgray")
+                .set("border-radius", "10px")
+                .set("margin", "10px");
+
+        return group;
+
     }
 
 }
